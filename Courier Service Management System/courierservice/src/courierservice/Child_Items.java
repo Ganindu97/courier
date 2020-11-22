6464f9bbd0f52fd32d5bd89d5b7a2e3f3dd30167
ganindu97 <ganindu123bilesh@gmail.com>
2020-11-22 19:43:50 +0530

    retrive data

---
+++ Courier Service Management System/courierservice/src/courierservice/Child_Items.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oo_inheritance_s;
+
+/**
+ *
+ * @author Savindu Nimsara
+ */
+public class Child_Items  extends Parent_Items{
+     public double pQuantity=5;
+    public double pSpecial_casing1=5.5;
+    public double pSpecial_casing2=5.6;
+    
+    public double pAmount=8;
+    public double pCasing1=4.0;
+    public double pCasing2=5;
+    
+}
